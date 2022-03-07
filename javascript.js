bf3b21952832f64ecd174ca0ca834ad7a6b820e5
EngMohammad1 <101139542+EngMohammad1@users.noreply.github.com>
2022-03-07 21:47:14 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+/*
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+
+
+function big() {
+    for (var i = 0; i < 100; i++) {
+        box1.style.background = "red";
+
+        box1.style.margin = "50px";
+        box2.style.width = "250px";
+        box3.style.width = "250px";
+
+        box1.style.borderRadius = "95px";
+        for (var j = 0; j < 100; j++) {
+            box1.style.background = "yellow";
+        }
+    }
+
+
+}
+
+function small() {
+    box2.style.backgroundColor = " rgb(34, 3, 94)";
+    box2.style.borderWidth = "20px";
+    box2.style.fontSize = "20px"
+    box2.style.height = "500px";
+    box2.style.width = "500px";
+}
+*/
+var table1 = document.getElementById("table1");
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+var time = document.getElementById("time");
